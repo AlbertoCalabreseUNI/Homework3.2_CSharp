40a5b0a104b77c2a6757add40b0b92805ddaee8c
Alberto Calabrese <calabrese.1709532@studenti.uniroma1.it>
2021-10-13 23:03:53 +0200

    Created system. Only needs animation.

--- Homework3.2_CSharp/Form1.Designer.cs
+++ Homework3.2_CSharp/Form1.Designer.cs
@@ -30,12 +30,59 @@ namespace Homework3._2_CSharp
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.myPictureBox1 = new Homework3._2_CSharp.MyPictureBox(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.myPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myPictureBox1
+            // 
+            this.myPictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.myPictureBox1.Name = "myPictureBox1";
+            this.myPictureBox1.Size = new System.Drawing.Size(316, 385);
+            this.myPictureBox1.TabIndex = 0;
+            this.myPictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 391);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load Book";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(440, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(348, 426);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.myPictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.myPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MyPictureBox myPictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Homework3.2_CSharp/Form1.cs
+++ Homework3.2_CSharp/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,60 @@ namespace Homework3._2_CSharp
 {
     public partial class Form1 : Form
     {
+        public IEnumerable<String> book;
+        public Dictionary<String, int> words;
+        public List<String> blacklist;
         public Form1()
         {
             InitializeComponent();
+            book = File.ReadLines(@"Files\book.txt");
+            words = new Dictionary<String, int>();
+            blacklist = new List<String>();
+            populateBlacklist();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (String line in book)
+            {
+                var ws = line.Split();
+                foreach(String word in ws)
+                {
+                    if (word.Length < 1 ) continue;
+                    if (!words.ContainsKey(word))
+                        words.Add(word, 1);
+                    else
+                        words[word] += 1;
+                }
+            }
+            //LINQ to sort by value
+            var sortedDict = from entry in words where entry.Value > 1 orderby entry.Value descending select entry;
+            foreach(KeyValuePair<String,int> word in sortedDict.Take(10))
+            {
+                textBox1.AppendText(word.Key + " - word count: " + word.Value);
+                textBox1.AppendText(Environment.NewLine);
+            }
+        }
+
+        public void populateBlacklist()
+        {
+            blacklist.Add("the");
+            blacklist.Add("and");
+            blacklist.Add("for");
+            blacklist.Add("not");
+            blacklist.Add("has");
+            blacklist.Add("are");
+            blacklist.Add("have");
+            blacklist.Add("that");
+            blacklist.Add("with");
+            blacklist.Add("but");
+            blacklist.Add("was");
+            blacklist.Add("were");
+            blacklist.Add("this");
+            blacklist.Add("from");
+            blacklist.Add("its");
+            blacklist.Add("his");
+            blacklist.Add("her");
         }
     }
 }
